8c1a4f235a8e475c378f96b311ff57b65ef0af99
thpvrs <73680823+thpvrs@users.noreply.github.com>
2020-11-20 15:04:13 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,6 +1,5 @@
 <?php
 $conn = mysqli_connect('itf-lab-database.mysql.database.azure.com', 'pavarisa@itf-lab-database', '2002THtonhom', 'itflab');
-if (mysqli_connect_errno($conn)) {
 $sql = 'DELETE FROM guestbook WHERE ID = ' . $_GET['ID'] . '';
 
 ?>
@@ -11,7 +10,7 @@ $sql = 'DELETE FROM guestbook WHERE ID = ' . $_GET['ID'] . '';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>delete</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 
 <body>
@@ -27,7 +26,7 @@ $sql = 'DELETE FROM guestbook WHERE ID = ' . $_GET['ID'] . '';
             mysqli_close($conn);
             ?>
         </h3>
-        <a href="index.php" class="btn btn-secondary mt-3">BACK</a>
+        <a href="index.php" class="btn btn-danger mt-3">OK</a>
 
     </div>
 </body>
