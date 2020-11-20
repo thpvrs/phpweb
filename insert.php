e694c3dbe1c2ff765cdc23c7d31a1585f8ce13a5
thpvrs <73680823+thpvrs@users.noreply.github.com>
2020-11-20 14:37:50 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -2,9 +2,8 @@
 
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'itf-lab-database.mysql.database.azure.com', 'pavarisa@itf-lab-database', '2002THtonhom', 'itflab', 3306);
-if (mysqli_connect_errno($conn))
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
+if (mysqli_connect_errno($conn)) {
+  die('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
 
@@ -13,14 +12,35 @@ $comment = $_POST['comment'];
 $link = $_POST['link'];
 
 
-$sql = "INSERT INTO guestbook (Name , comment , link) VALUES ('$name', '$comment', '$link')";
+$sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment', '$link')";
 
-
-if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
-  } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-  }
-  
-mysqli_close($conn);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>insert</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+
+<body>
+  <div class="container mt-5 text-center">
+      <h3>
+        <?php
+        if (mysqli_query($conn, $sql)) {
+          echo "New record created successfully";
+        } else {
+          echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+        mysqli_close($conn);
+        ?>
+      </h3>
+      <a href="index.php" class="btn btn-secondary mt-3">BACK</a>
+    </div>
+  </div>
+</body>
+
+</html>
